5c7d9af72ef5471c466ce77583a3b7fca7bdef52
JosephBracewell <40254519+JosephBracewell@users.noreply.github.com>
2018-10-18 20:59:06 -0400

    made enemies

--- Assets/_Scripts/BoundsCheck.cs
+++ Assets/_Scripts/BoundsCheck.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,44 +8,53 @@ using UnityEngine;
 /// Keeps a GameObject on screen.
 /// Note that this ONLY works for an orthographic Main Camera at [ 0, 0, 0 ].
 /// </summary>
-public class BoundsCheck : MonoBehaviour { // a
-	[Header("Set in Inspector")]
-	public float radius = 1f;
-
-	[Header("Set Dynamically")]
-	public float camWidth;
-	public float camHeight;
-
-	void Awake() {
-		camHeight = Camera.main.orthographicSize; // b
-		camWidth = camHeight * Camera.main.aspect; // c
-	}
-
-	void LateUpdate () { // d
-		Vector3 pos = transform.position;
-		if (pos.x > camWidth - radius) {
-			pos.x = camWidth - radius;
-		}
-
-		if (pos.x < -camWidth + radius) {
-			pos.x = -camWidth + radius;
-		} 
-
-		if (pos.y > camHeight - radius) {
-			pos.y = camHeight - radius;
-		}
-
-		if (pos.y < -camHeight + radius) {
-			pos.y = -camHeight + radius;
-		} 
-
-		transform.position = pos;
-	} 
-	
-	//Draw the bounds in the Scene pane using OnDrawGizmos()
-	void OnDrawGizmos () { // e
-		if (!Application.isPlaying) return;
-		Vector3 boundSize = new Vector3(camWidth* 2, camHeight* 2, 0.1f);
-		Gizmos.DrawWireCube(Vector3.zero, boundSize);
-	}
+public class BoundsCheck : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float radius = 1f;
+    public bool keepOnScreen = true; // a
+    [Header("Set Dynamically")]
+    public bool isOnScreen = true; // b
+    public float camWidth;
+    public float camHeight;
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize; // b
+        camWidth = camHeight * Camera.main.aspect; // c
+    }
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false; // b
+        if (pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+            offRight = true; // c
+        }
+        if (pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+            offLeft = true; // c
+        }
+        if (pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+            offUp = true; // c
+        }
+        if (pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+            offDown = true; // c
+        }
+        isOnScreen = !(offRight || offLeft || offUp || offDown); // d
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offUp = offDown = false; // e
+        }
+    }
 }
